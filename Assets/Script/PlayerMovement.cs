5ea67b42c2db4442965ded689c902b65f1b42d2e
mannat-bhardwaj <gameprogramer001@gmail.com>
2026-03-30 11:58:56 +0530

    PROJECT CREATED

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using Unity.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 7f;
+
+ 
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        
+        Vector2 movement = new Vector2(moveX, 0).normalized;
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        float tempx=Mathf.Clamp(transform.position.x,-8f,8f);
+    }
+
+}
